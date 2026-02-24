df389bf6986c8d8605d9e7003dc49cde547c8a5f
Ramirosued <ramirosued07@gmail.com>
2026-02-23 22:50:58 -0300

    actualizacion

--- backend/src/config/app.ts
+++ backend/src/config/app.ts
@@ -11,12 +11,13 @@ import gameRoutes from "../delivery/routes/gameRoutes";
 const app: Application = express();
 
 app.use(cors({
-    origin: (origin, callback) => {
-        callback(null, origin || true);
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+  origin: [
+    "http://localhost:5173",
+    "https://recursero-digital-ten.vercel.app"
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 
