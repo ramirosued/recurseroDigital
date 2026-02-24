f254f65354a528c21130dcae1d0e73d1caa1a5b2
Ramirosued <ramirosued07@gmail.com>
2026-02-23 23:58:13 -0300

    url

--- frontend/recursero-digital/src/infrastructure/config/api.js
+++ frontend/recursero-digital/src/infrastructure/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://recurserodigital-gzad.onrender.com/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://recurserodigital-gzad.onrender.com';
 
 export const AUTH_ENDPOINTS = {
   LOGIN_STUDENT: '/login/student',
