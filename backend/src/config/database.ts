749521da772ead7826cc88906d3ab9b73a17fc2a
Ramirosued <ramirosued07@gmail.com>
2026-02-24 13:48:50 -0300

    .

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,10 +1,5 @@
-export const databaseConfig = {
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false
-  }
-};
+import { Pool } from "pg";
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
